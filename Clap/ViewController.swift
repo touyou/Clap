d49a4bbd2c6edbab119656c7533f32dfaf2042c1
Fujii Yosuke <fujiyou1121@gmail.com>
2016-02-09 09:46:25 +0900

    add comment

--- Clap/ViewController.swift
+++ Clap/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let soundFilePath = NSBundle.mainBundle().pathForResource("clap", ofType: "wav")!
+        // とりあえず求められているのがString型なのでキャストして対応
         let fileURL = NSURL(fileURLWithPath: String(soundFilePath))
         do {
             audioPlayer = try AVAudioPlayer(contentsOfURL: fileURL)
@@ -34,19 +35,23 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         // Dispose of any resources that can be recreated.
     }
     
+    // PickerViewのセクションの数
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
     }
+    // PickerViewの選択肢の数
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 20
     }
+    // PickerViewの選択肢の文字列
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         return "\(row+1)回"
     }
+    // 選ばれた選択肢に応じてどうするか
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         soundCount = row
     }
-
+    // 選択肢の数だけ連続再生
     @IBAction func playButton() {
         audioPlayer.numberOfLoops = soundCount
         audioPlayer.play()
